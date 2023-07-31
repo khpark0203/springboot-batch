6864f0a721289304886c9742f29f904aed9df090
khpark0203 <khpark0203@gmail.com>
2023-07-31 09:05:10 +0900

    job service modify

--- src/main/java/com/khpark0203/batch/service/JobService.java
+++ src/main/java/com/khpark0203/batch/service/JobService.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.quartz.Job;
+import org.quartz.JobKey;
 import org.quartz.ObjectAlreadyExistsException;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.khpark0203.batch.config.JobConfig;
@@ -15,15 +15,14 @@ import com.khpark0203.batch.job.PrintJob;
 
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class JobService {
 
-    @Autowired
-    private Scheduler scheduler; // 쿼츠 스케줄 객체
-
-    @Autowired
-    private JobConfig jobConfig; // 쿼츠 스케줄 객체
+    private final Scheduler scheduler; // 쿼츠 스케줄 객체
+    private final JobConfig jobConfig; // 쿼츠 스케줄 객체
 
     @Getter
     public class Jobs {
@@ -32,28 +31,34 @@ public class JobService {
         private Map<? extends String, ? extends Object> map;
         private String exp;
         private String key;
+        private Boolean run;
 
-        public Jobs(Class<? extends Job> clazz, Map<? extends String, ? extends Object> map, String exp, String key) {
+        public Jobs(Class<? extends Job> clazz, Map<? extends String, ? extends Object> map, String exp, String key, Boolean run) {
             this.clazz = clazz;
             this.map = map;
             this.exp = exp;
             this.key = key;
+            this.run = run;
         }
         
     }
 
     private Jobs[] jobs = {
-        new Jobs(PrintJob.class, new HashMap<>(), "* * * * * ?", "PrintJob")
+        new Jobs(PrintJob.class, new HashMap<>(), "* * * * * ?", "PrintJob", true)
     };
     
     @PostConstruct
     public void run() {
         for (Jobs j: jobs) {
             try {
-                scheduler.scheduleJob(
-                    jobConfig.runJobDetail(j.getClazz(), j.getMap(), j.getKey()),
-                    jobConfig.runJobTrigger(j.getExp())
-                );
+                if (j.run) {
+                    scheduler.scheduleJob(
+                        jobConfig.runJobDetail(j.getClazz(), j.getMap(), j.getKey()),
+                        jobConfig.runJobTrigger(j.getExp())
+                    );
+                } else {
+                    scheduler.deleteJob(new JobKey(j.key, "DEFAULT"));
+                }
             } catch (ObjectAlreadyExistsException e) {
             } catch (SchedulerException e) {
                 e.printStackTrace();
